e3efee8643181ad71a269f07963094d27e4a7db3
Edgar <edgrr.trem000@gmail.com>
2020-01-05 17:01:55 +0700

    build usecase

--- lib/core/use_case.dart
+++ lib/core/use_case.dart
@@ -1,1 +1,13 @@
-abstract class UseCase<P, R> {}
+import 'package:daily_beer_goal_fl/core/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<P, R> {
+  Future<Either<Failure, R>> _execute(P params);
+
+  call(P params, Function(Either<Failure, R>) onResult) async {
+    final result = await _execute(params);
+    onResult(result);
+  }
+
+  Stream<Either<Failure, R>> stream(P params) => _execute(params).asStream();
+}
