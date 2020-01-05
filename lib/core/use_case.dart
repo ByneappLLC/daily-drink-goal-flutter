c2960198c14aebdb66d600844ec9ada939fb8cd4
Edgar <edgrr.trem000@gmail.com>
2020-01-05 17:10:32 +0700

    create get drinks usecase

--- lib/core/use_case.dart
+++ lib/core/use_case.dart
@@ -2,12 +2,12 @@ import 'package:daily_beer_goal_fl/core/failure.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class UseCase<P, R> {
-  Future<Either<Failure, R>> _execute(P params);
+  Future<Either<Failure, R>> execute(P params);
 
   call(P params, Function(Either<Failure, R>) onResult) async {
-    final result = await _execute(params);
+    final result = await execute(params);
     onResult(result);
   }
 
-  Stream<Either<Failure, R>> stream(P params) => _execute(params).asStream();
+  Stream<Either<Failure, R>> stream(P params) => execute(params).asStream();
 }
---
+++ lib/usecase/get_drinks_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:daily_beer_goal_fl/core/failure.dart';
+import 'package:daily_beer_goal_fl/core/use_case.dart';
+import 'package:daily_beer_goal_fl/data/database/beers_db.dart';
+import 'package:daily_beer_goal_fl/data/models/drink.dart';
+import 'package:dartz/dartz.dart';
+
+class GetDrinks extends UseCase<int, List<Drink>> {
+  final BeersDb _beersDb;
+
+  GetDrinks(this._beersDb);
+  @override
+  Future<Either<Failure, List<Drink>>> execute(int params) {
+    // TODO: implement execute
+    return null;
+  }
+}
