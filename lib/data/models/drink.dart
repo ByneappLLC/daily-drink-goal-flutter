84dee383d9b70b52dd9d7f673bfa8944b76f69f6
Edgar <edgrr.trem000@gmail.com>
2020-01-05 16:37:41 +0700

    craetea functions for database

--- lib/data/models/drink.dart
+++ lib/data/models/drink.dart
@@ -10,4 +10,20 @@ class Drink {
   final int date;
 
   Drink(this.amount, this.date);
+
+  Drink.fromMap(Map<String, dynamic> map)
+      : id = map[COLUMN_ID],
+        amount = map[COLUMN_AMOUNT],
+        date = map[COLUMN_DATE];
+
+  Map<String, dynamic> toMap() {
+    final map = {
+      COLUMN_AMOUNT: amount,
+      COLUMN_DATE: date,
+    };
+    if (id != null) {
+      map.putIfAbsent(COLUMN_ID, () => id);
+    }
+    return map;
+  }
 }
